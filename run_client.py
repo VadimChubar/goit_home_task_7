3ce47d083c90d17dbe64dbd5892ed3437690c2cb
VadimChubar <105510927+VadimChubar@users.noreply.github.com>
2022-11-19 17:11:42 +0200

    Add files via upload

---
+++ run_client.py
@@ -0,0 +1,26 @@
+import socket
+
+PORT = 8800
+TCP_IP = '127.0.0.1'
+
+
+def run_client(ip, port):
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_sock.connect((ip, port))
+    message = input('->>: ')
+    try:
+        while True:
+            if message.casefold().strip() == 'exit' or message.strip() == '':
+                break
+            client_sock.send(message.encode('utf-8'))
+            message_from_server = client_sock.recv(128)
+            print(f'Received message from a server: {message_from_server.decode("utf-8")}')
+            message = input('->>: ').casefold().strip()
+    except KeyboardInterrupt as e:
+        print(f'KeyboardInterrupt: {e}')
+    finally:
+        client_sock.close()
+
+
+if __name__ == '__main__':
+    run_client(TCP_IP, PORT)
---
+++ run_server.py
@@ -0,0 +1,29 @@
+import socket
+
+PORT = 8800
+IP = '127.0.0.1'
+
+
+def run_server(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((ip, port))
+    sock.listen(1)
+    print(f'Server is ready on {ip}:{port}')
+    connection, address = sock.accept()
+    try:
+        while True:
+            message_from_client = connection.recv(128)
+            if not message_from_client:
+                break
+            print(f'Received a message from a client: {message_from_client.decode("utf-8")}')
+            massage = input('->>: ')
+            connection.send(massage.encode('utf-8'))
+    except KeyboardInterrupt as e:
+        print(f'KeyboardInterrupt: {e}')
+    finally:
+        connection.close()
+        sock.close()
+
+
+if __name__ == '__main__':
+    run_server(IP, PORT)
